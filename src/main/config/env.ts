a1af3636322543314ab4b2271aebe77e72061fd8
Alan <allan_net@live.com>
2022-03-31 18:13:52 -0300

    refactor: enviromnent variables

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: global.__MONGO_URI__ || process.env.MONGO_URL,
+  mongoUrl: process.env.MONGO_URL || global.__MONGO_URI__,
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 }
