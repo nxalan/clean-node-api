a4a2218cd21bb5727c61c3bd7b9339665188757b
Alan <allan_net@live.com>
2022-03-31 18:35:05 -0300

    chore: add dotenv

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
+import * as dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
 setupMiddlewares(app)
 setupRoutes(app)
 export default app
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,8 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 export default {
-  mongoUrl: process.env.MONGO_URL || global.__MONGO_URI__,
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 }
