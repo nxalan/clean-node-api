cf9644c956a733a1f1d4c346e0698639fa09d718
Alan <allan_net@live.com>
2022-04-04 17:07:28 -0300

    feat: ensure DbAddAccount returns null if LoadAccountByEmailRepository returns an account

--- src/data/usecases/authentication/db-authentication-protocols.ts
+++ src/data/usecases/authentication/db-authentication-protocols.ts
@@ -1,7 +1,6 @@
 export * from '../../../domain/models/account'
 export * from '../../../domain/usecases/authentication'
 export * from '../../protocols/db/account/load-account-by-email-repository'
-export * from '../../protocols/db/account/load-account-by-email-repository'
 export * from '../../protocols/criptography/hash-comparer'
 export * from '../../protocols/criptography/encrypter'
 export * from '../../protocols/db/account/update-token-access-repository'
