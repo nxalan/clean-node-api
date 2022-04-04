7d652480bb311338acd9a5ea3643ec6e37b3c70e
Alan <allan_net@live.com>
2022-04-04 19:27:23 -0300

    feat: ensure AddSurveyController returns 204 on success

--- src/presentation/helpers/http/http-helper.ts
+++ src/presentation/helpers/http/http-helper.ts
@@ -6,14 +6,14 @@ export const badRequest = (error: Error): HttpResponse => ({
   body: error
 })
 
-export const forbidden = (error: Error): HttpResponse => ({
-  statusCode: 403,
-  body: error
-})
-
 export const unauthorized = (): HttpResponse => ({
   statusCode: 401,
   body: new UnauthorizedError()
+})
+
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error
 })
 
 export const serverError = (error: Error): HttpResponse => ({
@@ -24,4 +24,9 @@ export const serverError = (error: Error): HttpResponse => ({
 export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
+})
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null
 })
