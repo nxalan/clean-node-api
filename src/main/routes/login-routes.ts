f7794df0835888cc682a2fc19b6affaf160e0d64
Alan <allan_net@live.com>
2022-04-04 16:09:57 -0300

    refactor: separate factories in folders

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
-import { makeSignUpController } from '../factories/signup/signup-factory'
-import { makeLoginController } from '../factories/login/login-factory'
+import { makeSignUpController } from '../factories/controllers/signup/signup-controller-factory'
+import { makeLoginController } from '../factories/controllers/login/login-controller-factory'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 
 export default (router: Router): void => {
