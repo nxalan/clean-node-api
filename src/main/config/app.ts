5e7ba54f3e1712e410783bfa1be1554ed384744c
Alan <allan_net@live.com>
2022-04-08 13:22:45 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
+import setupSwagger from './config-swagger'
 import * as dotenv from 'dotenv'
 
 const app = express()
 dotenv.config()
+setupSwagger(app)
 setupMiddlewares(app)
 setupRoutes(app)
 export default app
