44e0aded10b3b1d37e5ab6f1a3b7c428718ec0e9
Alan <allan_net@live.com>
2022-04-08 14:44:22 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,25 @@
+import { loginPath } from '@/main/docs/paths/login-path'
+import { accountSchema } from '@/main/docs/schemas/accout-schema'
+import { loginParamsSchema } from '@/main/docs/schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API de enquetes',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
