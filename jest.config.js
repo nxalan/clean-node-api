c28ee6d1a85eea14b9e0918ae53ad4bc7aadfb09
Alan <allan_net@live.com>
2022-05-04 23:44:43 -0300

    fix: coveralls ignore domain folder

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,11 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'
-    // '!**/test/**'
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/domain/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
