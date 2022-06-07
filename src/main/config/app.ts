fa526841d02768596fecf480228ba21da4d2b7e7
Alan Nicolas de Oliveira <allan_net@live.com>
2022-06-06 23:36:44 -0300

    feat: add route to serve static files

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
+import setupStaticFiles from './static-files'
 import setupSwagger from './config-swagger'
 import * as dotenv from 'dotenv'
 
 const app = express()
 dotenv.config()
+setupStaticFiles(app)
 setupSwagger(app)
 setupMiddlewares(app)
 setupRoutes(app)
