c3f16aada202d34edd3525887b7a15a0752d8d18
Alan Nicolas de Oliveira <aoliveira.external@axa-assistance.us>
2022-06-18 17:41:12 -0300

    refactor: add type to controller requests

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,19 +1,19 @@
-import { Middleware, HttpRequest, HttpResponse } from '@/presentation/protocols'
+import { Middleware, HttpResponse } from '@/presentation/protocols'
 import { forbidden, ok, serverError } from '@/presentation/helpers'
 import { AccessDeniedError } from '@/presentation/errors'
 import { LoadAccountByToken } from '@/domain/usecases'
 
 export class AuthMiddleware implements Middleware {
   constructor (
-    private readonly LoadAccountByToken: LoadAccountByToken,
+    private readonly loadAccountByToken: LoadAccountByToken,
     private readonly role?: string
-  ) { }
+  ) {}
 
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+  async handle (request: AuthMiddleware.Request): Promise<HttpResponse> {
     try {
-      const accessToken = httpRequest.headers?.['x-access-token']
+      const { accessToken } = request
       if (accessToken) {
-        const account = await this.LoadAccountByToken.load(accessToken, this.role)
+        const account = await this.loadAccountByToken.load(accessToken, this.role)
         if (account) {
           return ok({ accountId: account.id })
         }
@@ -22,5 +22,11 @@ export class AuthMiddleware implements Middleware {
     } catch (error) {
       return serverError(error)
     }
+  }
+}
+
+export namespace AuthMiddleware {
+  export type Request = {
+    accessToken?: string
   }
 }
