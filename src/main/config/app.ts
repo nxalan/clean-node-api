efc2bef8627af5d23403ce06229f74c757f7549b
Alan Nicolas de Oliveira <aoliveira.external@axa-assistance.us>
2022-06-19 17:49:20 -0300

    refactor: change ci-cd to github actions

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -3,9 +3,11 @@ import setupMiddlewares from '@/main/config/middlewares'
 import setupRoutes from '@/main/config/routes'
 import setupStaticFiles from '@/main/config/static-files'
 import setupSwagger from '@/main/config/swagger'
+import * as dotenv from 'dotenv'
 
 export const setupApp = async (): Promise<Express> => {
   const app = express()
+  dotenv.config()
   setupStaticFiles(app)
   setupSwagger(app)
   setupMiddlewares(app)
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/survey-node-api',
   port: process.env.PORT || 5050,
